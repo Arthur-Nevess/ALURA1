3c4a87b162864e80bf429e3f74d9a1fca3223d0b
Arthur-Nevess <arthurneves0a12@gmail.com>
2024-02-04 21:10:43 -0300

    Finalizar apenas logica do desafio

--- Alura1/ex3.c
+++ Alura1/ex3.c
@@ -1,26 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-
-typedef struct pessoa
-{
-    float altura;
-    int idade;
-    char nome[50];
-    
-};
 
 int main()
 {
-    FILE *texto;
-
-    texto = fopen("texto1.txt", "w+");
-
-    if (texto != NULL)
-    {
-        fprintf(texto, "%s", "Primeiro código de manipulação de arquivo");
-    }
     
+
+    //variaveis do jogo
+    char jogada;
+    char machine[4];
+    sprintf(machine, "plt");
+    srand(time(0));
+    int operador = rand();
+    int machinechute=operador%3;
+    int usuario;
+    int rodada=0;
+    int jogador=0;
+    int maquina=0;
+
+
+
+    puts("=====================");
+    puts("Pedra, papel, tesoura");
+    puts("=====================");
+
+
+    puts("INSTRUÇÕES: PARA PEDRA DIGITE (P), PARA PAPEL (L), PARA TESOURA DIGITE (T)");
+
+    for(int i=0; i < 5; i++)
+    {
+
+       
+        printf("jogada %d de 5\n", i);
+        puts("_____________________");
+        printf("Digite sua jogada:");
+        scanf(" %c", &jogada);
+       
+        
+        //EMPATE
+        if(jogada==machine[machinechute])
+        {
+            puts("Empate");
+            continue;
+        }
+        //MAQUINA GANHANDO
+        else if(jogada=='l'&& machine[machinechute]=='t'||jogada=='t' && machine[machinechute]=='p'||jogada =='p'&& machine[machinechute]=='l')
+        {
+            puts("A MAQUINA GANHOU");
+            maquina++;
+        } 
+        //USUARIO GANHANDO      
+        else if(jogada=='t'&& machine[machinechute]=='l'||jogada=='p' && machine[machinechute]=='t'||jogada =='l'&& machine[machinechute]=='p')
+        {
+            puts("VOCÊ GANHOU");
+            jogador++;
+        }
+
+         printf("MAQUINA %d VOCÊ %d\n", maquina, jogador);
+        
+    }
 
     return 0;
 } 
